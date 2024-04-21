e477b3e6eccea83155532a9e6b1c7648ee79df98
David Carmichael <carmichaelits@gmail.com>
2024-04-21 16:53:13 +0100

    feat: update demos to include api examples

--- app_flask_demo/__init__.py
+++ app_flask_demo/__init__.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 from vite_transporter.platform.flask import ViteTransporter
 
@@ -8,11 +8,21 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    ViteTransporter(app, cors_allow_all=True)
+    app.secret_key = "flask_secret"
+    ViteTransporter(app, cors_allowed_hosts=["http://127.0.0.1:5003"])
 
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/api")
+    def api():
+        return {"message": "Hello, World!"}
+
+    @app.route("/api/session")
+    def session_():
+        session["message"] = "Hello, World!"
+        return {"session_message": session["message"]}
 
     return app
 
--- app_quart_demo/__init__.py
+++ app_quart_demo/__init__.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from quart import Quart, render_template
+from quart import Quart, render_template, session
 
 from vite_transporter.platform.quart import ViteTransporter
 
@@ -8,11 +8,21 @@ load_dotenv()
 
 def create_app():
     app = Quart(__name__)
-    ViteTransporter(app, cors_allow_all=True)
+    app.secret_key = "quart_secret"
+    ViteTransporter(app, cors_allowed_hosts=["http://127.0.0.1:5003"])
 
     @app.route("/")
     async def index():
         return await render_template("index.html")
+
+    @app.route("/api")
+    def api():
+        return {"message": "Hello, World!"}
+
+    @app.route("/api/session")
+    def session_():
+        session["message"] = "Hello, World!"
+        return {"session_message": session["message"]}
 
     return app
 
