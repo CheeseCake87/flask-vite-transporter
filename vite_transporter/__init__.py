335d8f23ac06b2520182b079dd14a6f121cb00ac
David Carmichael <carmichaelits@gmail.com>
2024-04-21 16:54:15 +0100

    chore: version increase

--- vite_transporter/__init__.py
+++ vite_transporter/__init__.py
@@ -5,7 +5,7 @@ from .helpers import PyProjectConfig as _PyProjectConfig
 from .helpers import _compile
 from .parser import ArgumentParser as _ArgumentParser
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 def _cli():
