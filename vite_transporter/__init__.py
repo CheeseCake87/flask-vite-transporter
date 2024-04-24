8318e16463e8a3dfd4efcdf56c3bedd1de7bfbc5
David Carmichael <carmichaelits@gmail.com>
2024-04-24 11:44:21 +0100

    chore: version increase

--- vite_transporter/__init__.py
+++ vite_transporter/__init__.py
@@ -5,7 +5,7 @@ from .utilities import Colr as _Colr
 from .utilities import PyProjectConfig as _PyProjectConfig
 from .utilities import compiler
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 
 def setup_subparsers(parser):
